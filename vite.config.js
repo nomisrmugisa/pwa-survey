89d06b7fb64ad01ff68284aa3b9da4542a28df88
Simon Kununka <skununka@hispuganda.org>
2026-02-18 13:45:45 +0300

    Configure allowedHosts for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,25 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // Expose to network
+    port: 5173,
+    allowedHosts: ['qimsdev.5am.co.bw', 'localhost', '127.0.0.1', '.5am.co.bw'], // Allow specific hosts
     proxy: {
       '/api': {
         target: 'https://qimsdev.5am.co.bw/qims',
         changeOrigin: true,
         secure: false,
+        configure: (proxy, options) => {
+          proxy.on('error', (err, req, res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            // console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, res) => {
+            // console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
       }
     }
   }
