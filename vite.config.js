10de24d8579011ce1255573a4f4d683690dabbac
Simon Kununka <skununka@hispuganda.org>
2026-02-18 13:51:16 +0300

    Set base path to /pwa-survey/

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pwa-survey/',
   server: {
     host: '0.0.0.0', // Expose to network
     port: 5173,
